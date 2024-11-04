e0c1056cbd9b6db45eb8d023465c20a8fb01e7f0
heronoa <heron.amaral@gmail.com>
2024-11-04 17:50:01 -0300

    Revert "feat: adding db testing"
    This reverts commit cda0b83dca29a836269ed3ee0e4e7106d99ab4ed.

--- test/jest.setup.ts
+++
@@ -1,7 +0,0 @@
-// jest.setup.js
-import { config } from 'dotenv';
-
-// Load the correct environment file depending on NODE_ENV
-const envFile =
-  process.env.NODE_ENV === 'test' ? '.env.test' : '.env.development';
-config({ path: envFile });
